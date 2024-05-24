7d50c1f155bd62bbf2af8b5ff8794e03ee3e33ee
Adaś <adampekul@gmail.com>
2024-05-24 19:02:50 +0200

    update hero style, add image slider

--- src/app/Pages/Home/hero/hero.component.ts
+++ src/app/Pages/Home/hero/hero.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent implements OnInit {
+  images = [
+    '../../../../assets/Images/hero/cake.jpg',
+    '../../../../assets/Images/hero/dumpling.jpg',
+    '../../../../assets/Images/hero/taco.jpg',
+    '../../../../assets/Images/hero/bowl.jpg',
+  ];
+  currentImageIndex = 0;
+
+  ngOnInit() {
+    this.startSlider();
+  }
+
+  startSlider() {
+    setInterval(() => {
+      this.currentImageIndex++;
+      if (this.currentImageIndex >= this.images.length) {
+        this.currentImageIndex = 0;
+      }
+    }, 10000);
+  }
+}
