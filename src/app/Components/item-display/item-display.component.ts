3dd6015e7b07f1c5033bb06459533fdf18c2a266
Adaś <adampekul@gmail.com>
2024-06-26 17:29:00 +0200

    update homepage content

--- src/app/Components/item-display/item-display.component.ts
+++ src/app/Components/item-display/item-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-display',
@@ -7,13 +8,25 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './item-display.component.css',
 })
 export class ItemDisplayComponent {
-  @Input() items: { id: string; name: string; img: string; rate: string }[] =
-    [];
+  @Input() items: {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+    rate: string;
+  }[] = [];
   @Input() title: string = '';
   faStar = faStar;
   stars = Array(5).fill(0);
 
+  constructor(private router: Router) {}
+
   getStarColor(index: number, rate: number) {
     return index < rate ? 'rgb(250, 204, 21)' : 'rgb(156, 163, 175)';
+  }
+
+  navigateToRecipeDetails(recipe: any) {
+    this.router.navigate(['/recipe', recipe.idMeal], {
+      state: { recipe: recipe },
+    });
   }
 }
--- src/app/Pages/Home/home.component.ts
+++ src/app/Pages/Home/home.component.ts
@@ -4,4 +4,53 @@ import { Component } from '@angular/core';
   selector: 'home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  popular = [
+    {
+      idMeal: '52995',
+      strMeal: 'BBQ Pork Sloppy Joes',
+      strMealThumb: '../../../../assets/Images/hero/BBQ_Pork_Sloppy_Joes.jpg',
+      rate: '4.5',
+    },
+    {
+      idMeal: '53032',
+      strMeal: 'Tonkatsu pork',
+      strMealThumb: '../../../../assets/Images/hero/Tonkatsu_pork.jpg',
+      rate: '4.2',
+    },
+    {
+      idMeal: '52770',
+      strMeal: 'Spaghetti Bolognese',
+      strMealThumb: '../../../../assets/Images/hero/Spaghetti_Bolognese.jpg',
+      rate: '4.7',
+    },
+    {
+      idMeal: '52829',
+      strMeal: 'Grilled Mac and Cheese Sandwich',
+      strMealThumb:
+        '../../../../assets/Images/hero/Grilled_Mac_and_Cheese_Sandwich.jpg',
+      rate: '4.0',
+    },
+  ];
+
+  sweet = [
+    {
+      idMeal: '52857',
+      strMeal: 'Pumpkin Pie',
+      strMealThumb: '../../../../assets/Images/hero/Pumpkin_Pie.jpg',
+      rate: '4.5',
+    },
+    {
+      idMeal: '52891',
+      strMeal: 'Blackberry Fool',
+      strMealThumb: '../../../../assets/Images/hero/Blackberry_Fool.jpg',
+      rate: '4.2',
+    },
+    {
+      idMeal: '52855',
+      strMeal: 'Banana Pancakes',
+      strMealThumb: '../../../../assets/Images/hero/Banana_Pancakes.jpg',
+      rate: '4.7',
+    },
+  ];
+}
